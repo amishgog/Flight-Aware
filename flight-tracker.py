7f3b7cc2b309b7fa9280d61aca146a8fc2a674e4
amishgog <111068373+amishgog@users.noreply.github.com>
2023-05-31 16:50:12 +0530

    Add files via upload

---
+++ flight-tracker.py
@@ -0,0 +1,59 @@
+import os
+import time
+import requests
+from json.decoder import JSONDecodeError
+import pandas as pd
+import random
+
+# Tracking area
+lat_min, lat_max = 18.465208, 58.465208
+long_min, long_max = 47.076825, 87.076825
+
+home_lat = "*****"
+home_long = "*****"
+
+# Function to fetch flight data
+def fetch_flight_data():
+    # API request URL
+    url_data = 'https://opensky-network.org/api/states/all?' + 'lamin=' + str(lat_min) + '&lomin=' + str(
+        long_min) + '&lamax=' + str(lat_max) + '&lomax=' + str(long_max)
+    try:
+        response = requests.get(url_data).json()
+
+        col_name = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'long', 'lat',
+                    'baro_altitude', 'on_ground', 'velocity']
+        flight_df = pd.DataFrame(response['states'])
+        flight_df = flight_df.loc[:, 0:9]
+        flight_df.columns = col_name
+        longitude = flight_df['long']
+        latitude = flight_df['lat']
+        call_sign = flight_df['callsign']
+        altitude = flight_df['baro_altitude']
+        origin = flight_df['origin_country']
+        velocity = flight_df['velocity']
+        for long, lat, sign, alt, orig, vel in zip(longitude, latitude, call_sign, altitude, origin, velocity):
+            if pd.notna(alt) and pd.notna(long) and pd.notna(lat) and pd.notna(orig) and pd.notna(vel):
+                if abs(lat - home_lat) <= 0.08 and abs(long - home_long) <= 0.08 and orig != "India": 
+                    send_notification(sign, alt, orig, vel)
+    except JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+
+# Function to send notification
+def send_notification(call_sign, altitude, origin, velocity):
+    notification_title = "\bTHERE IS AN AIRCRAFT OVERHEAD!!"
+    notification_text = f"-Flight : {call_sign} \n-Altitude : {altitude} m \n-Origin : {origin} \n-Velocity : {velocity} m/s"
+    tracking_url = f"https://flightaware.com/live/flight/{call_sign}"
+    sounds = ['Basso', 'Blow', 'Bottle', 'Frog', 'Funk', 'Glass', 'Hero', 'Morse', 'Ping', 'Pop', 'Purr', 'Sosumi', 'Submarine', 'Tink']
+    notification_sound = random.choice(sounds)
+
+    os.system(f"""
+        osascript -e 'display notification "{notification_text}" sound name "{notification_sound}" with title "{notification_title}"'
+        osascript -e 'open location "{tracking_url}"'
+    """)
+
+    time.sleep(10)
+
+# Main program loop
+while True:
+    fetch_flight_data()
+    time.sleep(60)  # Fetch flight data every minute
